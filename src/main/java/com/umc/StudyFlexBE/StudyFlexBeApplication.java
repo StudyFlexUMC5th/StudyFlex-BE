340cf688560ec661fa2aaf85eb89eedd1d424a6b
suhoon <jobak4200nim@nate.com>
2023-12-30 16:22:03 +0900

    Chore: 프로젝트 설정 - spring boot 3.1.7 - java 17 - lombok - spring data jpa - spring security

---
+++ src/main/java/com/umc/StudyFlexBE/StudyFlexBeApplication.java
@@ -0,0 +1,13 @@
+package com.umc.StudyFlexBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlexBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFlexBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/StudyFlexBE/StudyFlexBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.StudyFlexBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFlexBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
