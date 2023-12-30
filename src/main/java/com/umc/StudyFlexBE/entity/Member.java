92e4438609b4728f863cc92f44d9318eb66b57ae
suhoon <jobak4200nim@nate.com>
2023-12-30 18:51:47 +0900

    Refactor: Member 테이블 사이즈 설정

--- src/main/java/com/umc/StudyFlexBE/entity/Member.java
+++ src/main/java/com/umc/StudyFlexBE/entity/Member.java
@@ -7,9 +7,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Entity
 @AllArgsConstructor
@@ -21,13 +21,13 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long member_id;
 
-    @Column(name = "email")
+    @Column(name = "email", length = 100)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", length = 100)
     private String password;
 
-    @Column(name = "web_email")
+    @Column(name = "web_email", length = 100)
     private String web_email;
 
     @Column(name = "role")
