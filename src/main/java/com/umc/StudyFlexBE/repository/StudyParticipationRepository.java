672a1edf072cbd42fc928d3b22f675f430798df0
jansoon <ckdals4600@naver.com>
2024-01-06 01:04:38 +0900

    Feat: Repository 어노테이셔 추가

--- src/main/java/com/umc/StudyFlexBE/repository/StudyParticipationRepository.java
+++ src/main/java/com/umc/StudyFlexBE/repository/StudyParticipationRepository.java
@@ -4,7 +4,9 @@ import com.umc.StudyFlexBE.entity.Member;
 import com.umc.StudyFlexBE.entity.Study;
 import com.umc.StudyFlexBE.entity.StudyParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudyParticipationRepository extends JpaRepository<StudyParticipation, Long> {
     boolean existsByStudyAndMember(Study study, Member member);
 }
