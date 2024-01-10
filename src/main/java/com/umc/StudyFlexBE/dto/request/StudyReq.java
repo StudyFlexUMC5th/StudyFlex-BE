5c35ae0b0de036ccc392a7f4b95dff4a2ed0eea6
jansoon <ckdals4600@naver.com>
2024-01-10 10:17:49 +0900

    Fix: Json 이름 매핑 오류 수정

--- src/main/java/com/umc/StudyFlexBE/dto/request/StudyReq.java
+++ src/main/java/com/umc/StudyFlexBE/dto/request/StudyReq.java
@@ -1,5 +1,6 @@
 package com.umc.StudyFlexBE.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,14 +10,14 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyReq {
-    @Column(name = "thumbnail_url")
+    @JsonProperty("thumbnail_url")
     private MultipartFile thumbnailUrl;
-    @Column(name = "study_name")
+    @JsonProperty("study_name")
     private String studyName;
-    @Column(name = "max_members")
+    @JsonProperty("max_members")
     private int maxMembers;
-    @Column(name = "category_name")
+    @JsonProperty("category_name")
     private String categoryName;
-    @Column(name = "target_week")
+    @JsonProperty("target_week")
     private int targetWeek;
 }
